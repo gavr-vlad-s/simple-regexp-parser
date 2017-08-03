e786382c77aca4ede545dfb42204fa98c07f5713
gvs1977 <vladimir.s.gavrilov@gmail.com>
2017-08-03 15:39:28 +0300

    Added main file.

---
+++ Makefile
@@ -0,0 +1,26 @@
+LINKER        = g++
+LINKERFLAGS   =  
+COMPILER      = g++
+COMPILERFLAGS =  -std=c++14 -Wall -g
+BIN           = test-simple-parser
+LIBS          = -lboost_filesystem -lboost_system
+vpath %.cpp src
+vpath %.o build
+OBJ           = test-simple-parser.o simple_regex_parser.o get_init_state.o print_char32.o search_char.o sets_for_classes.o expr_scaner.o error_count.o aux_expr_scaner.o char_conv.o file_contents.o char_trie.o fsize.o
+LINKOBJ       = build/test-simple-parser.o build/simple_regex_parser.o build/get_init_state.o build/print_char32.o build/search_char.o build/sets_for_classes.o build/expr_scaner.o build/error_count.o build/aux_expr_scaner.o build/char_conv.o build/file_contents.o build/char_trie.o build/fsize.o
+
+.PHONY: all all-before all-after clean clean-custom
+
+all: all-before $(BIN) all-after
+
+clean: clean-custom 
+	rm -f ./build/*.o
+	rm -f ./build/$(BIN)
+
+.cpp.o:
+	$(COMPILER) -c $< -o $@ $(COMPILERFLAGS) 
+	mv $@ ./build
+
+$(BIN):$(OBJ)
+	$(LINKER) -o $(BIN) $(LINKOBJ) $(LIBS) $(LINKERFLAGS)
+	mv $(BIN) ./build
---
+++ src/test-simple-parser.cpp
@@ -0,0 +1,79 @@
+/*
+     File:    test-simple-parser.cpp
+     Created: 03 August 2017 at 15:33 Moscow time.
+     Author:  Гаврилов Владимир Сергеевич
+     E-mails: vladimir.s.gavrilov@gmail.com
+              gavrilov.vladimir.s@mail.ru
+              gavvs1977@yandex.ru
+*/
+
+#include <cstdlib>
+#include <cstdio>
+#include <memory>
+#include <string>
+// #include "../include/fsize.h"
+// #include "../include/error_count.h"
+// #include "../include/location.h"
+// #include "../include/trie.h"
+// #include "../include/aux_expr_scaner.h"
+// #include "../include/char_conv.h"
+// #include "../include/test_aux_expr_scaner.h"
+// #include "../include/errors_and_tries.h"
+#include "../include/file_contents.h"
+// #include "../include/char_trie.h"
+// #include "../include/expr_scaner.h"
+// #include "../include/test_expr_scaner.h"
+
+/* This function opens a file with test text, and it returns string containing text,
+ * if file is successfully opened and the file size is not equal to zero, and the
+ * empty string otherwise. */
+std::u32string init_testing(const char* name){
+    auto contents = get_contents(name);
+    auto str      = contents.second;
+    switch(contents.first){
+        case Get_contents_return_code::Normal:
+            if(!str.length()){
+                puts("File length is zero.");
+                return U"";
+            }else{
+                return utf8_to_u32string(str.c_str());
+            }
+            break;
+
+        case Get_contents_return_code::Impossible_open:
+            puts("It is impossible to open the file.");
+            return U"";
+
+        case Get_contents_return_code::Read_error:
+            puts("File reading failed.");
+            return U"";
+    }
+    return U"";
+}
+
+int main(int argc, char** argv) {
+//     if(1 == argc){
+//         puts("Test file is not given.");
+//     }else{
+//         std::u32string t = init_testing(argv[1]);
+//         if(t.length()){
+//             char32_t* p        = const_cast<char32_t*>(t.c_str());
+//             auto      loc      = std::make_shared<Location>(p);
+//
+//             Errors_and_tries etr;
+//             etr.ec             = std::make_shared<Error_count>();
+//             etr.ids_trie       = std::make_shared<Char_trie>();
+//             etr.strs_trie      = std::make_shared<Char_trie>();
+//             auto aesc          = std::make_shared<Aux_expr_scaner>(loc,  etr);
+// // #define TEST_AUX_SCANER
+// #ifdef TEST_AUX_SCANER
+//             test_aux_scaner(aesc);
+// #else
+//             auto trie_for_sets = std::make_shared<Trie_for_set_of_char32>();
+//             auto esc           = std::make_shared<Expr_scaner>(loc, etr, trie_for_sets);
+//             test_expr_scaner(esc, trie_for_sets);
+// #endif
+//         }
+//     }
+    return 0;
+}
