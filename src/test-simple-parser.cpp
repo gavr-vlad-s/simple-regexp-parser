d63df0f4e3661843612a1bc5826858ef513603c0
gvs1977 <vladimir.s.gavrilov@gmail.com>
2017-08-03 22:16:08 +0300

    Draft of 'int main' is written.

--- src/test-simple-parser.cpp
+++ src/test-simple-parser.cpp
@@ -13,15 +13,17 @@
 #include <string>
 // #include "../include/fsize.h"
 // #include "../include/error_count.h"
-// #include "../include/location.h"
+#include "../include/location.h"
 // #include "../include/trie.h"
-// #include "../include/aux_expr_scaner.h"
+#include "../include/aux_expr_scaner.h"
 #include "../include/char_conv.h"
 // #include "../include/test_aux_expr_scaner.h"
-// #include "../include/errors_and_tries.h"
+#include "../include/errors_and_tries.h"
 #include "../include/file_contents.h"
-// #include "../include/char_trie.h"
-// #include "../include/expr_scaner.h"
+#include "../include/char_trie.h"
+#include "../include/expr_scaner.h"
+#include "../include/simple_regex_parser.h"
+#include "../include/command.h"
 // #include "../include/test_expr_scaner.h"
 
 /* This function opens a file with test text, and it returns string containing text,
@@ -54,28 +56,26 @@ std::u32string init_testing(const char* name)
 
 int main(int argc, char** argv)
 {
-//     if(1 == argc){
-//         puts("Test file is not given.");
-//     }else{
-//         std::u32string t = init_testing(argv[1]);
-//         if(t.length()){
-//             char32_t* p        = const_cast<char32_t*>(t.c_str());
-//             auto      loc      = std::make_shared<Location>(p);
-//
-//             Errors_and_tries etr;
-//             etr.ec             = std::make_shared<Error_count>();
-//             etr.ids_trie       = std::make_shared<Char_trie>();
-//             etr.strs_trie      = std::make_shared<Char_trie>();
-//             auto aesc          = std::make_shared<Aux_expr_scaner>(loc,  etr);
-// // #define TEST_AUX_SCANER
-// #ifdef TEST_AUX_SCANER
-//             test_aux_scaner(aesc);
-// #else
-//             auto trie_for_sets = std::make_shared<Trie_for_set_of_char32>();
-//             auto esc           = std::make_shared<Expr_scaner>(loc, etr, trie_for_sets);
-//             test_expr_scaner(esc, trie_for_sets);
-// #endif
-//         }
-//     }
+    if(1 == argc){
+        puts("Test file is not given.");
+    }else{
+        std::u32string t = init_testing(argv[1]);
+        if(t.length()){
+            char32_t* p        = const_cast<char32_t*>(t.c_str());
+            auto      loc      = std::make_shared<Location>(p);
+
+            Errors_and_tries etr;
+            etr.ec             = std::make_shared<Error_count>();
+            etr.ids_trie       = std::make_shared<Char_trie>();
+            etr.strs_trie      = std::make_shared<Char_trie>();
+            auto aesc          = std::make_shared<Aux_expr_scaner>(loc,  etr);
+            auto trie_for_sets = std::make_shared<Trie_for_set_of_char32>();
+            auto esc           = std::make_shared<Expr_scaner>(loc, etr, trie_for_sets);
+            auto expr_parser   = std::make_unique<Simple_regex_parser>(esc, etr);
+
+            Command_buffer commands;
+            print_commands(commands);
+        }
+    }
     return 0;
 }
