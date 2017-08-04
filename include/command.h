ad684c4f34476e7cb84c2eed37f4be54ebf1dc74
gvs1977 <vladimir.s.gavrilov@gmail.com>
2017-08-04 18:45:12 +0300

    The function 'print_command' is written.

--- include/command.h
+++ include/command.h
@@ -32,32 +32,5 @@ struct Command{
     };
 };
 
-// enum Command_name {
-//     Cmd_or,             Cmd_concat,   Cmd_Kleene,
-//     Cmd_positive,       Cmd_optional, Cmd_char_def,
-//     Cmd_char_class_def, Cmd_unknown,  Cmd_multior,
-//     Cmd_multiconcat
-// };
-//
-// enum Char_class {
-//     C_Latin,    C_Letter,       C_Russian,
-//     C_bdigits,  C_digits,       C_latin,
-//     C_letter,   C_odigits,      C_russian,
-//     C_xdigits,  C_ndq,          C_nsq
-// };
-//
-// struct Command {
-//     size_t action_name; /* The index of the identifier that is the name of
-//                          * the action, in the prefix tree of identifiers. */
-//     Command_name name;
-//     union{
-//         struct {
-//             size_t first, second;
-//         } args;
-//         char32_t   c;
-//         Char_class cls;
-//     };
-// };
-
 using Command_buffer = std::vector<Command>;
 #endif
--- src/print_commands.cpp
+++ src/print_commands.cpp
@@ -10,7 +10,64 @@
 #include <cstdio>
 #include "../include/print_commands.h"
 #include "../include/operations_with_sets.h"
+#include "../include/print_char32.h"
+
+// enum class Command_name{
+//     Or,                    Concat,  Kleene,     Positive,
+//     Optional,              Char,    Char_class, Unknown,
+//     Char_class_complement, Multior, Multiconcat
+// };
+//
+// struct Command{
+//     size_t action_name; /* The index of the identifier that is the name of
+//                          * the action, in the prefix tree of identifiers. */
+//     Command_name name;
+//     union{
+//         struct {
+//             size_t first, second;
+//         } args;
+//         char32_t   c;
+//         size_t     idx_of_set;
+//     };
+// };
+
+static const char* command_names[] = {
+    "Or",                    "Concat",  "Kleene",     "Positive",
+    "Optional",              "Char",    "Char_class", "Unknown",
+    "Char_class_complement", "Multior", "Multiconcat"
+};
+
+void print_command(const Command& e, const Trie_for_set_of_char32ptr& t)
+{
+    printf("%s, ", command_names[static_cast<size_t>(e.name)]);
+    printf("action_name: %zu ",e.action_name);
+    switch(e.name){
+        case Command_name::Or:         case Command_name::Concat:
+        case Command_name::Multior:    case Command_name::Multiconcat:
+        case Command_name::Kleene:     case Command_name::Positive:
+        case Command_name::Optional:
+            {
+                auto args = e.args;
+                printf("{first_arg: %zu, second_arg: %zu}", args.first, args.second);
+            }
+            break;
+        case Command_name::Char:
+            print_char32(e.c);
+            break;
+        case Command_name::Char_class: case Command_name::Char_class_complement:
+            {
+                const auto& s = t->get_set(e.idx_of_set);
+                operations_with_sets::print_set(s, print_char32);
+            }
+            break;
+        default:
+            ;
+    }
+}
 
 void print_commands(const Command_buffer& buf, const Trie_for_set_of_char32ptr& t)
 {
+    for(const auto& e : buf){
+        print_command(e, t);
+    }
 }
